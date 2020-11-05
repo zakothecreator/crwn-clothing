8ce83e49c9565970fe9cba3e526602a9b0db8502
Zakothecreator <zakoaziz1@gmail.com>
2020-11-05 14:30:04 +0100

    i've added navigation using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import './App.css';
 import React from 'react';
+import {Link, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+
+const HatsPage = () => (
+  <div>
+  <h1>HATS PAGE</h1>
+    </div>
+);
+
+
 
 function App() {
   return (
     <div>
-    <HomePage />
+    
+    <Route exact path='/' component={HomePage} />
+    <Route path='/hats' component={HatsPage} />
+   
     </div>
   );
   
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component{
                 {
                     title: 'Hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                   },
                   {
                     title: 'Jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
+                    id: 2,
+                    linkUrl:''
                   },
                   {
                     title: 'Sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
+                    id: 3,
+                    linkUrl:''
                   },
                   {
                     title: 'Womens',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl:''
                   },
                   {
                     title: 'Mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl:''
                   }
                 
             ]
@@ -46,8 +51,8 @@ class Directory extends React.Component{
         return(
             <div className='directory-menu'>
             {this.state.sections.map(
-                ({title, imageUrl, id, size}) => 
-                (<MenuItem title = {title} imageUrl = {imageUrl} key ={id} size = {size} />)
+                ({id, ...otherSectionProps }) => 
+                (<MenuItem key ={id} {...otherSectionProps} />)
             )}
             </div>
         );
